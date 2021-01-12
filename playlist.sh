d3672326592acda4370e5d413fb125ea7c691dbd
Ondrej Bima <ondra@bimovi.cz>
2021-01-12 15:07:42 +0100

    logo stanice, cislo kanalu a priprava pro epg

--- playlist.sh
+++ playlist.sh
@@ -10,6 +10,7 @@ if [ ! -f "${current_location}/config.file" ] ; then printf "ERROR: Config file 
 file=$(cat ${current_location}/config.file | head -n 1 )
 service=$(echo ${file} | cut -d ' ' -f1)
 script=$(echo ${file} | cut -d ' ' -f5)
+
 if [ ! -f "${current_location}/access.token" ] ; then printf "ERROR: Access token not found. Please run login.sh.\n" ; exit 1 ; fi
 access_token=$(cat ${current_location}/access.token | head -n 1 )
 token=$(echo ${access_token} | cut -d ' ' -f1)
@@ -19,7 +20,7 @@ playlist=${current_location}/playlist.general.m3u8
 streamer=${script}streamer.sh
 
 PREFIX=#EXTM3U
-PREFIX1ST=#EXTINF:-1,
+PREFIX1ST=#EXTINF:-1
 PREFIX2ND=pipe://${streamer}
 
 channels=$(curl -s --header "Authorization: Bearer ${token}" --header "Content-Type: application/json" "https://${service}.magio.tv/v2/television/channels?list=LIVE")
@@ -27,10 +28,11 @@ if [ $? != 0 ] ; then printf "ERROR: Unsuccessful channel list request.\n" ; exi
 channels=$(echo ${channels} | tr '\r\n' ' ')
 channels_success=$(echo ${channels} | jq -r '.success')
 if [ $channels_success != "true" ] ; then
-channels_error=$(echo ${channels} | jq -r '.errorMessage')
-echo "ERROR: Server responded: ${error}"
-exit 1
+	channels_error=$(echo ${channels} | jq -r '.errorMessage')
+	echo "ERROR: Server responded: ${error}"
+	exit 1
 fi
+
 channels=$(echo ${channels} | tr '\r\n' ' ')
 max=$(echo ${channels} | jq -r ".items" | jq -r ".|length")
 items=$(echo ${channels} | jq -r ".items")
@@ -41,7 +43,9 @@ while [ $i -lt $max ] ; do
 	channel=$(echo ${items} | jq -r ".[${i}].channel")
 	id=$(echo ${channel} | jq -r ".channelId")
 	name=$(echo ${channel} | jq -r ".name")
-	printf "${PREFIX1ST}%s\n" "${name}" >> ${playlist}
+	logo=$(echo ${channel} | jq -r ".logoUrl")
+	channel_num=$(echo ${channel} | jq -r ".defaultChannelPosition")
+	printf "${PREFIX1ST} tvg-logo='%s' tvh-chnum='%s' tvg-id='%s',%s\n" "${logo}" "${channel_num}" "${id}.${service}.magio.tv" "${name}" >> ${playlist}
 	printf "${PREFIX2ND} %s\n" "${id}" >> ${playlist}
 	i=$((i + 1))
 	printf "Generated %s channels.\r" $i
